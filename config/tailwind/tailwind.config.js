7359d508b1a7bb2c2adaa2dd44a49c6929db1be7
DevCeur <dev.carlos.umana@gmail.com>
2022-07-09 14:01:37 -0500

    [feat]: update tailwind config to implement app theme

--- config/tailwind/tailwind.config.js
+++ config/tailwind/tailwind.config.js
@@ -1,13 +1,24 @@
 module.exports = {
   content: ["./app/**/*.{ts,tsx,js,jsx}"],
 
-  darkMode: "class",
-
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        "bg-primary": "#F7F8FD",
+        "bg-secondary": "#FFFFFF",
+        "bg-overlay": "#F2F4FF",
+
+        "fg-primary": "#3A4374",
+        "fg-secondary": "#647196",
+        "fg-overlay": "#FFFFFF",
+
+        "brand-purple": "#AD1FEA",
+
+        "brand-orange": "#F49F85",
+
+        "brand-blue-primary": "#4661E6",
+        "brand-blue-secondary": "#62BCFA",
+      },
     },
   },
-
-  plugins: [],
 };
