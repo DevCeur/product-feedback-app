685e275df99908e914f31a85cb40e25f2609ed38
DevCeur <dev.carlos.umana@gmail.com>
2022-07-10 03:50:24 -0500

    [feat]: improve suggestions view responsiveness on mobile

--- app/components/MainLayout/MainLayout.tsx
+++ app/components/MainLayout/MainLayout.tsx
@@ -1,6 +1,6 @@
 export const MainLayout: React.FC = ({ children }) => {
   return (
-    <div className="w-[90%] min-h-screen max-w-[1110px] mx-auto pt-12 lg:pt-[94px] flex">
+    <div className="w-full md:w-[90%] min-h-screen max-w-[1110px] px-5 md:mx-auto pt-12 lg:pt-[94px] flex">
       <div className="flex-1">{children}</div>
     </div>
   );
--- app/views/SuggestionsView/SuggestionsView.tsx
+++ app/views/SuggestionsView/SuggestionsView.tsx
@@ -1,5 +1,5 @@
 import { Link } from "@remix-run/react";
-import { BiChevronDown } from "react-icons/bi";
+import { BiChevronDown, BiMenu } from "react-icons/bi";
 import { HiLightBulb, HiPlusSm } from "react-icons/hi";
 
 import { ProjectBanner } from "~/components/ProjectBanner";
@@ -22,8 +22,19 @@ const AddSuggestionLink = () => {
 
 export const SuggestionsView = () => {
   return (
-    <div className="w-full h-full flex flex-col lg:grid grid-cols-4 grid-flow-col gap-7">
-      <div className="w-full col-span-1 lg:flex flex-col lg:space-y-6 grid grid-cols-3 gap-2">
+    <div className="w-full h-full relative flex flex-col lg:grid grid-cols-4 grid-flow-col gap-7">
+      <div className="w-full fixed top-0 left-0 z-50 md:hidden px-6 py-5 bg-project-gradient bg-[length:1000px] bg-left-top flex justify-between items-center">
+        <div className="flex flex-col">
+          <h3 className="font-bold text-white mb-1">Frontend Mentor</h3>
+          <p className="text-xs text-fg-overlay">Feedback Board</p>
+        </div>
+
+        <button className="text-white text-4xl">
+          <BiMenu />
+        </button>
+      </div>
+
+      <div className="w-full col-span-1 hidden lg:flex flex-col lg:space-y-6 md:grid grid-cols-3 gap-2">
         <ProjectBanner />
 
         <FeedbackCategoriesFilter />
@@ -31,10 +42,10 @@ export const SuggestionsView = () => {
         <ProjectRoadmapBanner />
       </div>
 
-      <div className="w-full h-full col-span-3 overflow-hidden flex flex-col">
-        <div className="w-full bg-bg-overlay-dark text-white py-5 px-7 mb-7 flex items-center justify-between rounded-xl">
+      <div className="w-full h-full pt-[36px] md:pt-0 col-span-3 md:overflow-hidden flex flex-col">
+        <div className="md:w-full -mx-5 bg-bg-overlay-dark text-white py-3 px-6 md:mx-auto md:px-7 mb-7 flex items-center justify-between md:rounded-xl">
           <div className="flex items-center">
-            <div className="flex items-center space-x-4 mr-9">
+            <div className="hidden md:flex items-center space-x-4 mr-9">
               <HiLightBulb className="text-2xl" />
 
               <p className="text-lg font-bold">6 Suggestions</p>
