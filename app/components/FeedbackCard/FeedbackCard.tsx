d1f7834b081c279e6e972f0d55d4326fda54735d
DevCeur <dev.carlos.umana@gmail.com>
2022-07-10 14:26:03 -0500

    [feat]: improve feedback card component responsiveness

--- app/components/FeedbackCard/FeedbackCard.tsx
+++ app/components/FeedbackCard/FeedbackCard.tsx
@@ -2,7 +2,7 @@ import { Link } from "@remix-run/react";
 import { BiChevronUp } from "react-icons/bi";
 import { FaComment } from "react-icons/fa";
 
-export const FeedbackCategory = ({ category }: { category: string }) => {
+export const FeedbackCategoryBadge = ({ category }: { category: string }) => {
   return (
     <div className="inline-block py-2 px-4 lg:px-5 bg-bg-overlay transition-colors duration-200 rounded-xl">
       <span className="text-sm font-medium text-brand-blue-primary">
@@ -14,27 +14,39 @@ export const FeedbackCategory = ({ category }: { category: string }) => {
 
 export const FeedbackCard = () => {
   return (
-    <div className="w-full bg-white p-[28px] flex items-start justify-between space-x-10 rounded-xl">
-      <button className="px-3 py-2 flex flex-col items-center justify-center bg-bg-overlay hover:bg-bg-overlay-light rounded-xl transition-all duration-200">
-        <BiChevronUp className="text-xl text-brand-blue-primary" />
-        <span className="text-sm font-bold text-fg-primary">112</span>
-      </button>
+    <div className="w-full bg-white p-6 md:p-8 flex flex-col-reverse md:flex-row items-start justify-between md:space-x-10 rounded-xl">
+      <div className="w-full md:w-min flex justify-between">
+        <button className="px-3 py-2 flex md:flex-col items-center justify-center space-x-2 md:space-x-0 bg-bg-overlay hover:bg-bg-overlay-light rounded-xl transition-all duration-200">
+          <BiChevronUp className="text-xl text-brand-blue-primary" />
+
+          <span className="text-sm font-bold text-fg-primary">112</span>
+        </button>
+
+        <Link
+          to="/feedback-detail"
+          className="flex md:hidden items-center space-x-3"
+        >
+          <FaComment className="text-xl text-bg-overlay-light" />
+
+          <span className="font-bold text-fg-primary">2</span>
+        </Link>
+      </div>
 
       <Link
         to="/feedback-detail"
-        className="flex-1 flex items-center justify-between"
+        className="flex-1 mb-6 md:mb-0 flex items-center justify-between"
       >
         <div>
-          <p className="text-lg font-bold text-fg-primary mb-1">
+          <p className="md:text-lg font-bold text-fg-primary mb-1">
             Add Tags For Solutions
           </p>
-          <p className="text-fg-secondary mb-3">
+          <p className="text-sm text-fg-secondary mb-3">
             Easier to search for solutions based on a specific stack.
           </p>
-          <FeedbackCategory category="Enhancement" />
+          <FeedbackCategoryBadge category="Enhancement" />
         </div>
 
-        <div className="flex items-center space-x-3">
+        <div className="hidden md:flex items-center space-x-3">
           <FaComment className="text-xl text-bg-overlay-light" />
 
           <span className="font-bold text-fg-primary">2</span>
